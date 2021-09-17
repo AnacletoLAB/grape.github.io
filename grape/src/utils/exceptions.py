c5c0a325c0818f5368982d87e592d512880abe32
iimpulse <michael.gargano@jax.org>
2021-09-17 10:57:33 -0400

    add exception changes

--- grape/src/utils/exceptions.py
+++ grape/src/utils/exceptions.py
@@ -2,13 +2,12 @@
 Base exceptions namespace for the app
 """
 from werkzeug.exceptions import HTTPException
-
 from src.utils.logging import get_module_logger
 
 LOGGER = get_module_logger()
 
 
-class GrapeException(Exception):
+class GrapeBaseException(HTTPException):
     """
     Base exception for the grape app
     """
@@ -17,35 +16,10 @@ class GrapeException(Exception):
     detail = "This response means we couldn't generate a more specific error. Please" \
              "try again, but if the problem persists contact an application " \
              "administrator."
-    @classmethod
-    def restful_handler(cls, message=None):
-        """
-        This handler gets attached to flask-restplus as an errorhandler when then
-        register_all_exceptions function of the src.app.controller.utils
-        errorhandlers namespace is called.
-        Must be named 'restful_handler'
-        :param message: Message from the exception e.g. `ThisException('My Message')`
-        """
-        message = message or cls.message
-        LOGGER.info("Using handler for error %s - %s", cls.__name__, message)
-        return {'message': message, 'detail': cls.detail}
 
+class GrapeException(GrapeBaseException, HTTPException):
 
-
-class ImATeapotException(GrapeException, HTTPException):
-    """
-    Example exception demonstrating flask-restplus api errorhandler
-    """
-    message = 'Short and Stout'
-    @classmethod
-    def restful_handler(cls, message=None):
-        """
-        This handler gets attached to flask-restplus as an errorhandler when then
-        register_all_exceptions function of the src.app.controller.utils
-        errorhandlers namespace is called.
-        Must be named 'restful_handler'
-        :param message:
-        """
-        message = message or cls.message
-        LOGGER.info("Using handler for error %s - %s", cls.__name__, message)
-        return {'message': message}, 418
+    message = "Oops! Something went wrong and we couldn't process your request."
+    detail = "This response means we couldn't generate a more specific error. Please" \
+             "try again, but if the problem persists contact an application " \
+             "administrator."
